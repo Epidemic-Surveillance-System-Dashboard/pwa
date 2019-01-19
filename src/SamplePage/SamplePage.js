32389eb52c8b3ea11c9a5fc680fd5522ab94fbab
Laban <labanlin@me.com>
2019-01-18 19:06:58 -0500

    Started Visualizer component to simplify creation of graphs in the future

--- src/SamplePage/SamplePage.js
+++ src/SamplePage/SamplePage.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import '../../node_modules/react-vis/dist/style.css';
 import './SamplePage.css';
+import Visualizer from '../Visualizer/Visualizer'
+import {XYPlot, VerticalBarSeriesCanvas, XAxis, YAxis, HorizontalGridLines} from 'react-vis';
 
-class App extends Component {
+let metric = {
+    name: "Hello",
+    value: 10,
+    date: new Date ("November 1, 2018 00:00:00 GMT"),
+}
+
+class SamplePage extends Component {
+
+    transform(rawData){
+        let data = [
+            {x: rawData.name, y: rawData.value},
+        ]
+        return data
+    }
+
     render() {
+
+        let barData = [
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,0,1).getTime()/1000, value: Math.random() * 1000},
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,1,1).getTime()/1000, value: Math.random() * 1000},
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,2,1).getTime()/1000, value: Math.random() * 1000},
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,3,1).getTime()/1000, value: Math.random() * 1000}
+        ]
+
+        let pieData = [
+            {angle:50, label: "term 1", subLabel: "50%"},
+            {angle:40, label: "term 2"},
+            {angle:30, label: "term 3"},
+            {angle:20, label: "term 4"},
+            {angle:10, label: "term 5"},
+
+        ]
+        // let width = this.props.width ? this.props.width : 500
+        // let height = this.props.height ? this.props.height : 500
+        // let xDistance = this.props.xDistance ? this.props.xDistance : 100
+
         return (
             <div className="sampleApp">
-                <header className="sampleApp-header">
-                    <p>
-                        This is the sample page!
-                    </p>
-                </header>
+                <p>
+                    This contains sample graphs. 
+                    You can pass width, height, xInterval, type, and -- of course -- data to these Visualizer components.
+                </p>
+                <Visualizer data={barData} title = "Sample Bar Graph" type = "barchart"></Visualizer>
+                <Visualizer data={pieData} title = "Sample Pie Graph" type = "piechart"></Visualizer>
             </div>
         );
     }
 }
 
-export default App;
+export default SamplePage;
---
+++ src/Visualizer/Visualizer.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import '../../node_modules/react-vis/dist/style.css';
+import { XYPlot, VerticalBarSeriesCanvas, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, RadialChart,} from 'react-vis';
+
+
+class Visualizer extends Component {
+
+
+    /**
+     * Data Transform Utilities. Transforms DB JSON output into a usable format for the relevant graph type.
+     * Because we haven't integrated the tool to extract JSON from the DB yet, we are making some assumptions
+     * here about what the JSON will look like. As a result, these utiltiies are subject to change, though
+     * the graphs are unlikely to.
+     */
+
+    MetricOverTime() {
+        /**
+         * Input Format:
+         *      sortedData = {[
+         *              {metric:"metricName", dateTime: "unix timestamp", value: numberValue}
+         *              ]}
+         * 
+         * 
+         * Example:
+         * 
+         * 
+         *         let barData = [
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,0,1).getTime()/1000, value: Math.random() * 1000},
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,1,1).getTime()/1000, value: Math.random() * 1000},
+            {metric: "Vaccines Given Per Month", dateTime: new Date(2018,2,1).getTime()/1000, value: Math.random() * 1000}
+        ]
+
+         */
+
+        let series = []
+        let name = ""
+        for (let i = 0; i < this.props.data.length; i++){
+            let dataPoint = this.props.data[i]
+            if (i == 0) name = dataPoint.metric
+            let date = new Date(dataPoint.dateTime * 1000)            
+            let months = ["Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+            series.push({
+                x: `${months[date.getMonth()]} ${date.getFullYear()}`,
+                y: dataPoint.value
+            })
+        }
+        return {name: name, series: series}
+    }
+
+    BarChart(defaults) {
+        return (
+            <XYPlot xType="ordinal" width={defaults.width} height={defaults.height} xDistance={defaults.xDistance}>
+                <HorizontalGridLines />
+                <XAxis />
+                <YAxis />
+                <VerticalBarSeriesCanvas data={this.MetricOverTime().series}/>
+            </XYPlot>
+        )
+    }
+
+    PieChart(defaults){
+        return(
+            <RadialChart data = {this.props.data} width = {defaults.width} height = {defaults.height} showLabels={true} />
+        )
+    }
+
+    /**
+     * Types:
+     *      LineChart([Metric Over Time])
+     *      BarChart(Set of Metrics @ 1 time)
+     *      PieChart(Set of Metrics @ 1 time)
+     *      DoubleBarChart(Set of Sets)
+     */
+
+    render() {
+
+        //Define Defaults
+        let defaults = {
+            width: 500,
+            height: 500,
+            xDistance: 100,
+            title: "Graph"
+        }
+
+        //Override Defaults where appropriate
+        Object.keys(defaults).forEach((key) =>{
+            if (this.props[key] !== undefined) defaults[key] = this.props[key]
+        });
+        
+        let graph = null
+
+        switch(this.props.type){
+            case "barchart": 
+                graph = this.BarChart(defaults)
+            break;
+            case "piechart":
+                graph = this.PieChart(defaults)
+            break;
+        }
+         
+        if (graph !== null){
+            return (
+                <div className="Visualizer">
+                    {graph}
+                </div>
+            )
+        }else{
+            return (
+            <div className="Visualizer">
+                Sorry, something went wrong.
+            </div>)
+        }
+
+    }
+}
+
+export default Visualizer;
