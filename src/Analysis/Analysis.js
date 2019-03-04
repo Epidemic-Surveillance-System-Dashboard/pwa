7cf612d3b84c61df2564f8848798845620c3d3ba
trickly <patrick.lee.96@gmail.com>
2019-03-04 03:53:43 -0500

    added User-page like states in Analysis page. still need to work on return handler and the updateLocation function

--- src/Analysis/Analysis.js
+++ src/Analysis/Analysis.js
@@ -6,17 +6,19 @@ import LocationSelector from "../LocationSelector/LocationSelector"
 import MetricSelector from "../MetricSelector/MetricSelector"
 
 const Option = Select.Option
-const listData = [];
+var dataDict = {};
 for (let i = 0; i < 20; i++) {
     var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    listData.push({
+    var dataPoint = {
         color: color,
         facility: `facility ${i}`,
         avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
         state: `state ${i}`,
         LGA: `LGA ${i}`,
         ward: `Ward ${i}`
-    });
+    };
+    var dataList = [];
+    dataDict[dataPoint.facility + dataPoint.ward + dataPoint.LGA + dataPoint.state] = dataPoint;
 }
 
 
@@ -26,9 +28,15 @@ class Analysis extends Component {
         location: null,
         initLoading: false,
         loading: false,
-        list: listData,
+        data: dataDict,
+        //state-based stuff below
+        showTable: true,
+        selectedUser: null,
+        dataLoaded: false,
+        currentView: "table"
+
     }
-  
+
     updateLocation = (location) => {
         this.setState({ location: location })
     }
@@ -38,6 +46,28 @@ class Analysis extends Component {
     addLocation = () => {
 
     }
+
+    editLocation = (location) => {
+        /*
+        let newUser = dataSource.find(object => {
+            return object.Id === id
+        })*/
+
+        this.setState({
+            currentView: "existing",
+            //selectedUser: newUser
+        })
+
+    }
+    showTable = () => {
+        this.setState(
+            { currentView: "table" }
+        )
+    }
+    showHideTableClass = () => {
+        return this.state.currentView === "table" ? "" : "displayNone"
+    }
+
     render() {
         const { initLoading, loading, list } = this.state;
         const loadMore = !initLoading && !loading ? (
@@ -47,55 +77,71 @@ class Analysis extends Component {
         ) : null;
 
         return (
-            <div className="center">
-                <Row className={``} gutter={16}>
-                    <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
-                        <Card className="left" size="medium" title="Select Metric">
-                            <MetricSelector parentHandler = {this.updateLocation} showData = {true}></MetricSelector>
-                            <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange()}>
-                                <Option value="jack">Jack</Option>
-                                <Option value="lucy">Lucy</Option>
-                                <Option value="disabled" disabled>Disabled</Option>
-                                <Option value="Yiminghe">yiminghe</Option>
-                            </Select>
-                        </Card>
-                    </Col>
-                </Row>
-                <Divider />
-                <Row className={``} gutter={16}>
-                    <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
-                        <Card className="left" size="medium" title="Select Location">
-                            <List
-                                itemLayout="horizontal"
-                                size="large"
-                                pagination={{
-                                    onChange: (page) => {
-                                        console.log(page);
-                                    },
-                                    pageSize: 4,
-                                }}
-                                dataSource={list}
-                                renderItem={item => (
-                                    <List.Item
-                                        key={item.facility}
-                                        actions={[,
-                                            <Button style={{ marginLeft: 8 }}>
-                                                Edit <Icon type="edit" />
-                                            </Button>
-                                        ]}>
-                                        <List.Item.Meta
-                                            avatar={<Avatar icon="environment" style={{ backgroundColor: item.color }} />}
-                                            title={<a href="https://ant.design">{item.facility}</a>}
-                                            description={
-                                                item.state + " / " + item.LGA + " / " + item.ward
-                                            }
-                                        />
-                                    </List.Item>
-                                )}
-                            />
-                        </Card>
-                    </Col>
-                </Row>
+            <div>
+
+                {
+                    this.state.currentView === "table" &&
+                    <div className="center">
+                        <Row className={``} gutter={16}>
+                            <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
+                                <Card className="left" size="medium" title="Select Metric">
+                                    <MetricSelector parentHandler={this.updateLocation} showData={true}></MetricSelector>
+                                    <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange()}>
+                                        <Option value="jack">Jack</Option>
+                                        <Option value="lucy">Lucy</Option>
+                                        <Option value="disabled" disabled>Disabled</Option>
+                                        <Option value="Yiminghe">yiminghe</Option>
+                                    </Select>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Divider />
+                        <Row className={``} gutter={16}>
+                            <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
+                                <Card className="left" size="medium" title="Select Location">
+                                    <List
+                                        itemLayout="horizontal"
+                                        size="large"
+                                        pagination={{
+                                            onChange: (page) => {
+                                                console.log(page);
+                                            },
+                                            pageSize: 4,
+                                        }}
+                                        dataSource={Object.values(dataDict)}
+                                        renderItem={item => (
+                                            <List.Item
+                                                key={item.facility}
+                                                actions={[,
+                                                    <Button onClick={() => { this.editLocation(item) }} style={{ marginLeft: 8 }}>
+                                                        Edit <Icon type="edit" />
+                                                    </Button>
+                                                ]}>
+                                                <List.Item.Meta
+                                                    avatar={<Avatar icon="environment" style={{ backgroundColor: item.color }} />}
+                                                    title={<a href="https://ant.design">{item.facility}</a>}
+                                                    description={
+                                                        item.state + " / " + item.LGA + " / " + item.ward
+                                                    }
+                                                />
+                                            </List.Item>
+                                        )}
+                                    />
+                                </Card>
+                            </Col>
+                        </Row>
+                    </div>
+                }
+
+
+                {this.state.currentView !== "existing" ?
+                    null :
+                    <div className="">
+                        <LocationSelector
+                            parentHandler={this.updateLocation} showLocation={false}
+                        />
+                    </div>
+                }
             </div>
         );
     }
