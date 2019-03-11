6b19b574ab9e07b233ebb435c1c32594c8de321d
Jackie Ngo <jngo42@uwo.ca>
2019-03-11 12:00:04 -0400

    working

--- src/Users/CreateModifyDeleteUser.js
+++ src/Users/CreateModifyDeleteUser.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Button, Input, Row, Col, Divider, Popconfirm, message, Select} from 'antd'
+import { Button, Input, Row, Col, Divider, Popconfirm, message, Select } from 'antd'
 
 import LocationSelector from "../LocationSelector/LocationSelector"
 
@@ -19,59 +19,69 @@ const userFields = [
 
 class CreateModifyDeleteUser extends Component {
 
-    componentDidMount(){
-        if (this.props.mode === "new") 
+    componentDidMount() {
+        if (this.props.mode === "new")
             this.enableEditing()
+        
+            user.user().then((result) => {
+                this.setState({
+                    ready: true,
+                    loggedInUser: result
+                })
+            })
+        
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.setState({
             userInfo: this.computedState(this.props.user),
             passedUser: this.props.user
-        })      
+        })
     }
+    
 
     componentDidUpdate(oldProps) {
         const newProps = this.props
-        if(oldProps.user !== newProps.user) {
+        if (oldProps.user !== newProps.user) {
             this.setState({
                 passedUser: this.props.user,
                 userInfo: this.computedState(newProps.user)
             })
         }
-        
+
     }
 
-    inputLabelTab = (text) =>{
-        return(
-            <div style={{minWidth: "90px", textAlign:"right"}}>
+    inputLabelTab = (text) => {
+        return (
+            <div style={{ minWidth: "90px", textAlign: "right" }}>
                 {text}
             </div>
         )
     }
-    
+
     state = {
         mode: this.props.mode ? this.props.mode : "view", //View (default unless overridden), create, or edit,
         userChanged: false,
         disabled: true,
-        locationDisabled: true
+        locationDisabled: true,
+        loggedInUser: null
         //userInfo holds current computed properties (including modifications)
         //passedUser holds the original user information
     }
 
-    computedState(user){
+    computedState(user) {
 
         let userState = {}
 
-        if (user == null){
-            for (let i = 0; i < userFields.length; i++){
+        if (user == null) {
+            for (let i = 0; i < userFields.length; i++) {
                 userState[userFields[i]] = null
             }
-        }else{
-            for (let i = 0; i < userFields.length; i++){
-                if (userFields[i] in this.props.user){
+        } else {
+            for (let i = 0; i < userFields.length; i++) {
+                if (userFields[i] in this.props.user) {
                     userState[userFields[i]] = this.props.user[userFields[i]]
-                }else{
+                } else {
                     userState[userFields[i]] = null
                 }
             }
@@ -81,53 +91,53 @@ class CreateModifyDeleteUser extends Component {
 
     //If this is the user, then they can change the password.
     //Otherwise, this is an admin, who can reset a password.
-    isUser = () =>{
+    isUser = () => {
         return true
     }
 
-    confirmDelete = async () =>{
+    confirmDelete = async () => {
         let url = `https://essd-backend-dev.azurewebsites.net/api/users/deleteUser/${this.state.passedUser.Id}`
         //Send Delete HTTP Request
-        let deleteRequest = await fetch(url, {method: "delete"})
-        deleteRequest.json().then((data) =>{
-            db.User.delete(this.state.passedUser.Id).then(() =>{
+        let deleteRequest = await fetch(url, { method: "delete" })
+        deleteRequest.json().then((data) => {
+            db.User.delete(this.state.passedUser.Id).then(() => {
                 message.success("Successfully deleted user.")
                 this.props.refreshUsers()
             })
         })
-        .catch((error) =>{
-            message.error("Sorry, something went wrong.")
-        })
+            .catch((error) => {
+                message.error("Sorry, something went wrong.")
+            })
     }
 
-    adminFeatures = () =>{
+    adminFeatures = () => {
         //Only show delete this is a secondary view (ie showTable_f exists)
-        if (this.props.user != null && this.props.showTable_f !== null) return(
+        if (this.props.user != null && this.props.showTable_f !== null) return (
             <div>
                 <Col className="right">
                     <Popconfirm placement="topRight" title="Are you sure you want to delete this user? This action cannot be reverted." onConfirm={this.confirmDelete} okText="Delete" cancelText="Cancel">
-                        <Button type ="danger">Delete User</Button>
+                        <Button type="danger">Delete User</Button>
                     </Popconfirm>
                 </Col>
 
             </div>
         )
         return null
-    }   
+    }
 
     ///Todo: generate function to update state
 
-    inputChanged = (stateName,e) =>{
+    inputChanged = (stateName, e) => {
         let user = this.state.userInfo
         user[stateName] = e.target.value
-        this.setState({userInfo: user})
+        this.setState({ userInfo: user })
         this.userInformationChanged()
     }
 
-    handleUserTypeSelect(value){
+    handleUserTypeSelect(value) {
         let user = this.state.userInfo
         user["UserType"] = value
-        this.setState({userInfo: user})
+        this.setState({ userInfo: user })
         //this.userInformationChanged()
         console.log(this.state.userInfo);
     }
@@ -143,42 +153,66 @@ class CreateModifyDeleteUser extends Component {
 
         let array = []
 
-        for (let i = 0; i < basicFeatures.length; i++){
+        for (let i = 0; i < basicFeatures.length; i++) {
             let featureName = basicFeatures[i]
-            let featureNameKey = featureName.replace(" ","")   //e.g. "First Name" -> user.FirstName
+            let featureNameKey = featureName.replace(" ", "")   //e.g. "First Name" -> user.FirstName
             array.push(
-                <Input addonBefore = {this.inputLabelTab(featureName)}
-                    value = {this.state.userInfo ? this.state.userInfo[featureNameKey] : ""}
+                <Input addonBefore={this.inputLabelTab(featureName)}
+                    value={this.state.userInfo ? this.state.userInfo[featureNameKey] : ""}
                     disabled={this.state.disabled}
-                    key = {i}
-                    onChange = {(e) =>{this.inputChanged(featureNameKey,e)}}/>
+                    key={i}
+                    onChange={(e) => { this.inputChanged(featureNameKey, e) }} />
             )
-        }  
+        }
 
-        return(
-            <Col>
-                <Divider/>
-                {array}
-                <Select style={{width: "100%"}} defaultValue={this.state.userInfo.UserType} placeholder="User Type" onChange={(e) =>{this.handleUserTypeSelect(e)}} disabled = {this.state.disabled}>
+        let allUserTypeOptions = [];
+
+        switch (this.state.userInfo.UserType) {
+            case "superadmin":
+                allUserTypeOptions = [
+                    <Select.Option value="superadmin">Super Admin</Select.Option>,
+                    <Select.Option value="admin">Admin</Select.Option>,
                     <Select.Option value="user">User</Select.Option>
-                    <Select.Option value="admin">Admin</Select.Option>
-                    <Select.Option value="superadmin">Super Admin</Select.Option>
+                ];
+                break;
+            case "admin":
+                allUserTypeOptions = [
+                    <Select.Option value="admin">Admin</Select.Option>,
+                    <Select.Option value="user">User</Select.Option>
+                ];
+                break;
+            case "user":
+                allUserTypeOptions = [
+                    <Select.Option value="user">User</Select.Option>
+                ];
+                break;
+        }
+
+        //var loggedinUser = await user.user();
+        //console.log(loggedinUser);
+
+        return (
+            <Col>
+                <Divider />
+                {array}
+                <Select style={{ width: "100%" }} defaultValue={this.state.userInfo.UserType} placeholder="User Type" onChange={(e) => { this.handleUserTypeSelect(e) }} disabled={this.state.loggedInUser.Email == this.state.userInfo.Email ? true : this.state.disabled}>
+                    {allUserTypeOptions}
                 </Select>
-                <Divider/>
+                <Divider />
                 {/* Location */}
                 <p>Location</p>
                 {/* Todo: add max scope depending on admin rights*/}
-                <LocationSelector 
-                    parentHandler = {this.updateLocation} 
-                    showLocation = {true} 
-                    initialLocation = {{Id: this.state.userInfo.LocationId, Type :this.state.userInfo.LocationType}}
-                    disabled={this.state.disabled}/>
+                <LocationSelector
+                    parentHandler={this.updateLocation}
+                    showLocation={true}
+                    initialLocation={{ Id: this.state.userInfo.LocationId, Type: this.state.userInfo.LocationType }}
+                    disabled={this.state.disabled} />
             </Col>
         )
 
     }
 
-    updateLocation = (location) =>{
+    updateLocation = (location) => {
         let userInfo = this.state.userInfo
         userInfo.LocationId = location.Id
         userInfo.LocationType = location.Type
@@ -190,66 +224,66 @@ class CreateModifyDeleteUser extends Component {
     modifyControls = () => {
         if (this.state.disabled) return (
             <div>
-                <Button onClick = {this.enableEditing}>Edit</Button>
+                <Button onClick={this.enableEditing}>Edit</Button>
             </div>
         );
         else return (
             <div>
-                <Button hidden = {this.state.mode === "new"} onClick = {this.cancelEditing}>Cancel</Button>
-                <Button disabled = {!this.state.userChanged} onClick = {this.save}>Save</Button>
+                <Button hidden={this.state.mode === "new"} onClick={this.cancelEditing}>Cancel</Button>
+                <Button disabled={!this.state.userChanged} onClick={this.save}>Save</Button>
             </div>
         )
     }
 
-    cancelEditing = () =>{
+    cancelEditing = () => {
         let user = this.computedState(this.state.passedUser)
         this.setState({
             userInfo: user,
-            disabled:true,
-            locationDisabled:true,
+            disabled: true,
+            locationDisabled: true,
         })
         this.userInformationChanged()
     }
 
     enableEditing = () => {
-        user.user().then(u =>{
-            if (u.userType === "admin"){
+        user.user().then(u => {
+            if (u.userType === "admin") {
                 this.setState({
-                    disabled:false,
-                    locationDisabled:false,
+                    disabled: false,
+                    locationDisabled: false,
                 })
-            }else{
+            } else {
                 this.setState({
-                    disabled:false,
-                    locationDisabled:true,
+                    disabled: false,
+                    locationDisabled: true,
                 })
             }
             this.userInformationChanged()
-        })   
+        })
     }
 
-    userInformationChanged = () =>{
+    userInformationChanged = () => {
         //Todo: error checking here for valid inputs
         let changed = false
         if (this.props.mode === "new") {
             changed = true
-        }else{
-            for (let i = 0; i < userFields.length; i++){
-                if (this.state.userInfo[userFields[i]] !== this.state.passedUser[userFields[i]]){
+        } else {
+            for (let i = 0; i < userFields.length; i++) {
+                if (this.state.userInfo[userFields[i]] !== this.state.passedUser[userFields[i]]) {
                     changed = true
                     break
                 }
             }
         }
-        this.setState({userChanged: changed})
+        this.setState({ userChanged: changed })
     }
 
-    save = async () =>{
+    save = async () => {
         let url, successMessage, errorMessage, method = ""
-        let successHandler = () => {}
+        let successHandler = () => { }
         let userObject = this.state.userInfo
 
-        if (this.state.mode === "new"){
+        if (this.state.mode === "new") {
             //Create User
             //userObject.UserType = "user" //hardCode for now
             delete userObject.Id
@@ -257,22 +291,22 @@ class CreateModifyDeleteUser extends Component {
             successMessage = "Successfully added user."
             errorMessage = "Failed to create user. Please try again later."
             method = "POST"
-            successHandler = (result) =>{
+            successHandler = (result) => {
                 console.log(result)
-                if ("Id" in result){
+                if ("Id" in result) {
                     userObject.Id = result.Id
-                    db.User.add(userObject).then(() =>{
+                    db.User.add(userObject).then(() => {
                         message.success(successMessage)
                         this.props.refreshUsers()
                         this.cancelEditing()
                     })
-                }else{
+                } else {
                     console.log('throwing an error')
                     throw new Error(result.error)
                 }
 
             }
-        }else{
+        } else {
 
             //Update User
             //userObject.UserType = "user" //hardCode for now
@@ -280,9 +314,9 @@ class CreateModifyDeleteUser extends Component {
             successMessage = "Successfully updated user."
             errorMessage = "Failed to update user. Please try again later."
             method = "PUT"
-            successHandler = (result) =>{
-                if (result.result === "Update successful"){                
-                    db.User.put(userObject).then(() =>{
+            successHandler = (result) => {
+                if (result.result === "Update successful") {
+                    db.User.put(userObject).then(() => {
                         message.success(successMessage)
                         this.props.refreshUsers()
                         this.cancelEditing()
@@ -300,43 +334,50 @@ class CreateModifyDeleteUser extends Component {
         })
 
         request.json().then(successHandler)
-        .catch((error) =>{
-            console.log(error)
-            message.error(errorMessage)
-        })
+            .catch((error) => {
+                console.log(error)
+                message.error(errorMessage)
+            })
 
     }
 
-    back = () =>{
+    back = () => {
         //Remove any changes before executing callback
         this.cancelEditing()
         this.props.showTable_f()
     }
-/**
- * Always render basic elements (first name, last name, etc)
- * If current user == user viewed => allow changing password
- * else if currentUser.type == admin || == superAdmin allow resetting password
- * TODO: implement this when auth starts to work.
- */
-    render(){
-        return(
+    /**
+     * Always render basic elements (first name, last name, etc)
+     * If current user == user viewed => allow changing password
+     * else if currentUser.type == admin || == superAdmin allow resetting password
+     * TODO: implement this when auth starts to work.
+     */
+    render() {
+        return (
             <div>
                 {
-                    this.props.showTable_f &&
-                    <Row className="rowVMarginSm ">
-                        <Col>
-                            <Button onClick = {this.back} icon="caret-left">Back</Button>
-                        </Col>
-                    </Row>
+                    this.state.ready &&
+                    <div>
+                        {
+                            this.props.showTable_f &&
+                            <Row className="rowVMarginSm ">
+                                <Col>
+                                    <Button onClick={this.back} icon="caret-left">Back</Button>
+                                </Col>
+                            </Row>
+                        }
+                        <Row className="rowVMarginSm">
+                            <h3>User Details</h3>
+                            {this.modifyControls()}
+                            {this.basicFeatures()}
+                            <Divider />
+                            {this.adminFeatures()}
+                        </Row>
+
+                    </div>
                 }
-                <Row className="rowVMarginSm">
-                    <h3>User Details</h3>
-                    {this.modifyControls()}
-                    {this.basicFeatures()}
-                    <Divider/>
-                    {this.adminFeatures()}
-                </Row>
-                
+
+
             </div>
         )
     }
