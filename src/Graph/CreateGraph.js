a9d9dd92ab0cc7e2a923c90758dc9d12fa9423cf
trickly <patrick.lee.96@gmail.com>
2019-03-13 00:58:15 -0400

    row/col in create graph

--- src/Graph/CreateGraph.js
+++ src/Graph/CreateGraph.js
@@ -4,6 +4,7 @@ import MetricSelector from '../MetricSelector/MetricSelector';
 import VisualizerManager from '../Visualizer/VisualizerManager';
 import SaveGraph from '../Graph/SaveGraph'
 import RangeSelector from '../RangeSelector/RangeSelector'
+import { Card, Row, Col, Divider } from 'antd'
 
 
 class CreateGraph extends Component {
@@ -27,8 +28,8 @@ class CreateGraph extends Component {
     updateData = (data) => {
         this.setState({ Data: data })
     }
-    updateDates =(dates)=>{
-        this.setState({Dates: dates});
+    updateDates = (dates) => {
+        this.setState({ Dates: dates });
     }
     createLocationObject = () => {
         let loc = this.state.Location
@@ -49,51 +50,78 @@ class CreateGraph extends Component {
     render() {
         return (
             <div>
-                {this.state.Data ? this.state.Data.Type : undefined}
-                <h3>Create a Graph</h3>
-                <h4>Select Location</h4>
-                <LocationSelector
-                    parentHandler={this.updateLocation}
-                    initialLocation={
-                        {
-                            Id: "1215",
-                            Type: "Facility"
-                        }
-                    }
-                />
-                <h4>Select Data</h4>
-                <MetricSelector
-                    parentHandler={this.updateData}
-                    initialData={{
-                        GroupValue: "1191|Facility Attendance|Group",
-                        SetValue: "-3-1191|All Facility Attendance (Distribution)|Group",
-                        MetricValue: ""
-                    }}
-                />
-                <RangeSelector
-                parentHandler={this.updateDates}
-                initialData={
-                    {
-                        Dates: this.state.Dates
-                    }
-                }
-                />
-                <VisualizerManager
-                    Title={this.state.Data !== undefined && this.state.Data.Name !== undefined ? this.state.Data.Name.split("(")[0] : ""}
-                    Location={this.state.Location} //{Name, Id, Type}
-                    Data={this.state.Data} // {Id, Type, TotalOrDistribution="total|none|distribution"}
-                    Dates={this.state.Dates}
-                    ParentHandler={this.updateRawData}
-                />
+                <Row className={``} gutter={16}>
+                    <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
+                        <Card className="left" size="medium" title="Create a Graph">
+                            <Row className={``} gutter={16}>
+                                <h4>Select Location</h4>
+                                <LocationSelector
+                                    parentHandler={this.updateLocation}
+                                    initialLocation={
+                                        {
+                                            Id: "1215",
+                                            Type: "Facility"
+                                        }
+                                    }
+                                />
+                            </Row>
+                            <Divider/>
+                            <Row className={``} gutter={16}>
 
-                <SaveGraph
-                    Data={this.state.Data}
-                    Dates={this.state.Dates}
-                    Locations={this.createLocationObject()}
-                    Title={this.getTitle()}
-                    RawData={this.state.RawData}
-                    ParentHandler={this.props.ParentHandler}
-                />
+                                <h4>Select Data</h4>
+                                <MetricSelector
+                                    parentHandler={this.updateData}
+                                    initialData={{
+                                        GroupValue: "1191|Facility Attendance|Group",
+                                        SetValue: "-3-1191|All Facility Attendance (Distribution)|Group",
+                                        MetricValue: ""
+                                    }}
+                                />
+                            </Row>
+                            <Divider/>
+                            <Row className={``} gutter={16}>
+                            <h4>Select Date Range</h4>
+
+                                <RangeSelector
+                                    parentHandler={this.updateDates}
+                                    initialData={
+                                        {
+                                            Dates: this.state.Dates
+                                        }
+                                    }
+                                />
+                            </Row>
+
+
+                        </Card>
+                    </Col>
+                </Row>
+                <Divider />
+                <Row className={``} gutter={16}>
+                    <Col xs={{ span: 24, offset: 0 }} sm={{ span: 22, offset: 1 }} md={{ span: 18, offset: 3 }} lg={{ span: 16, offset: 4 }}>
+                        <Card className="left" size="medium" title="Your Graph">
+                            <div>
+                                <VisualizerManager
+                                    Title={this.state.Data !== undefined && this.state.Data.Name !== undefined ? this.state.Data.Name.split("(")[0] : ""}
+                                    Location={this.state.Location} //{Name, Id, Type}
+                                    Data={this.state.Data} // {Id, Type, TotalOrDistribution="total|none|distribution"}
+                                    Dates={this.state.Dates}
+                                    ParentHandler={this.updateRawData}
+                                />
+
+                                <SaveGraph
+                                    Data={this.state.Data}
+                                    Dates={this.state.Dates}
+                                    Locations={this.createLocationObject()}
+                                    Title={this.getTitle()}
+                                    RawData={this.state.RawData}
+                                    ParentHandler={this.props.ParentHandler}
+                                />
+                            </div>
+
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         )
     }
