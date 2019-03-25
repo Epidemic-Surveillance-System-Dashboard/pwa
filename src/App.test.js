711fd931e1084328b4a86764e19ca18a12448a2a
Jackie Ngo <jngo42@uwo.ca>
2019-03-25 16:54:58 -0400

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Account from './Account/Account';
+import db from './Database/database';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders account without crashing', () => {
+  const div = document.createElement('username');
+  ReactDOM.render(<Account />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
